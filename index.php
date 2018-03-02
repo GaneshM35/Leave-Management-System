1ad96e2201ea707f24c2970c85e3283761e0380b
Ganesh M <ganesh.m635@gmail.com>
2018-03-02 22:53:37 +0530

    Login page Setup

--- index.php
+++ index.php
@@ -1,63 +1,66 @@
 <!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+<html>
+
+<head>
+    <title>Leave Management System</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>CSE-Hackathon</title>
+    <meta charset="utf-8">
+    <meta name="keywords" content="Kgkite Leave" />
+    <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css" media="all">
+    <link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+    <link href="//fonts.googleapis.com/css?family=Raleway:400,500,600,700,800,900" rel="stylesheet">
+</head>
 
-    
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
+<body>
+    <div class="signupform">
+        <h1>Leave Management System - KiTE</h1>
+        <div class="container">
+            <div class="agile_info">
+                <div class="w3l_form">
+                    <img src="images/login-banner.png">
+                </div>
+                <div class="w3_info">
+                    <h2>Log in with your credentials !!</h2>
+                    <p>
+                    	<?php 
+	                        $errors = array(
+	                            1=>"Invalid user name or password, Try again",
+	                            2=>"Please login to access this area"
+	                        );
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
-  <body>
-    <div class="container">
-      <div class="info">
-         <h2 class="bg-primary">Leave Request Form</h2>
-       
-          <div class="col-md-6 col-md-offset-3">
-                    <h4></span>Log in with your credentials<span class="glyphicon glyphicon-user"></h4><br/>
-                            <div class="block-margin-top">
-                              <?php 
+	                        $error_id = isset($_GET['err']) ? (int)$_GET['err'] : 0;
 
-                                $errors = array(
-                                    1=>"Invalid user name or password, Try again",
-                                    2=>"Please login to access this area"
-                                  );
+	                        if ($error_id == 1) {
+                                echo '<p class="text_danger">'.$errors[$error_id].'</p>';
+                            }elseif ($error_id == 2) {
+                                echo '<p class="text_danger">'.$errors[$error_id].'</p>';
+                            }
+	                    ?>  
+                    </p>
+                    <form action="Controllers/authenticate.php" method="post">
+                        <div class="input-group">
+                            <span><i class="fa fa-user" aria-hidden="true"></i></span>
+                            <input type="text" name="username" placeholder="Username" required autofocus>
+                        </div>
+                        <div class="input-group">
+                            <span><i class="fa fa-unlock" aria-hidden="true"><span>
+							</i><input type="Password" name="password" placeholder="Password" required>
+						</div>
+							<input type="checkbox" value="remember-me" /> <h4>Keep me signed in  </h4>        
+							<button class="btn btn-danger btn-block" type="submit">Sign In</button >                     
+					</form>
+			    </div>
+			<div class="clear"></div>
+			</div>
+			
+		</div>
+		<div class="footer">
 
-                                $error_id = isset($_GET['err']) ? (int)$_GET['err'] : 0;
+ <p>&copy; Project 2K18. All Rights Reserved | Design by Batch 10</p>
+ </div>
+	</div>
 
-                                if ($error_id == 1) {
-                                        echo '<p class="text-danger">'.$errors[$error_id].'</p>';
-                                    }elseif ($error_id == 2) {
-                                        echo '<p class="text-danger">'.$errors[$error_id].'</p>';
-                                    }
-                               ?>  
-
-                              <form action="authenticate.php" method="POST" class="form-signin col-md-8 col-md-offset-2" role="form">  
-                                  <input type="text" name="username" class="form-control" placeholder="User ID" required autofocus><br/>
-                                  <input type="password" name="password" class="form-control" placeholder="Password" required><br/>
-                                  <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-                             </form>
-                           </div>
-            </div>
-
-      </div>
-      
-     
-    </div>
-  
-
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="js/bootstrap.min.js"></script>
-  </body>
+	</body>
 </html>
